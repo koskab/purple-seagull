c5382661447b272f9081e4d5f478ec5bd9cd7ad4
koskab <koskab13@icloud.com>
2024-02-13 11:41:51 +0600

    Новый проект

---
+++ AlabsTest/src/main/java/com/example/AlabsTest/AlabsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.AlabsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlabsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlabsTestApplication.class, args);
+	}
+
+}
---
+++ AlabsTest/src/test/java/com/example/AlabsTest/AlabsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlabsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlabsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
