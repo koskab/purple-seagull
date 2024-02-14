80f78f27875544badbe527208ad49959dddb3d20
koskab <koskab13@icloud.com>
2024-02-14 16:50:26 +0600

    feat: Add DTOs for entities

---
+++ alabstest/src/main/java/items/DTO/ItemProperties.java
@@ -0,0 +1,21 @@
+package items.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemProperties {
+    private String name;
+    private LocalDate deadlineDate;
+    private int count;
+    private BigDecimal price;
+}
---
+++ alabstest/src/main/java/users/DTO/UserProperties.java
@@ -0,0 +1,17 @@
+package users.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProperties {
+    private LocalDateTime createdAt;
+    private String fullname;
+}
