fac72104ca2dc2b520eb4552e8b3e5f87fa25470
koskab <159869904+koskab@users.noreply.github.com>
2024-02-14 15:37:06 +0600

    Feat/add entities references (#2)
    feat: Update entities with Lombok

---
+++ alabstest/src/main/java/items/entity/Item.java
@@ -0,0 +1,54 @@
+package items.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import shops.entity.Shop;
+import users.entity.User;
+
+@Entity
+@Table(name = "items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deadline_date", nullable = false)
+    private LocalDate deadlineDate;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Byte isDeleted = 0;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "count")
+    private Integer count = 0;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price = BigDecimal.valueOf(0);
+}
