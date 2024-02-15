e1daa92418dc447e18f224100a561045945f0ad8
koskab <koskab13@icloud.com>
2024-02-15 15:54:22 +0600

    refactor: Remove unused imports

--- alabstest/src/main/java/com/example/alabstest/users/dto/UserUpdate.java
+++ alabstest/src/main/java/com/example/alabstest/users/dto/UserUpdate.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Getter
