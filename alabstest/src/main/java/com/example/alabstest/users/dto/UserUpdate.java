2a590fde0f24df83036b703261c3d772584a2555
koskab <koskab13@icloud.com>
2024-02-15 15:49:46 +0600

    refactor: Replace balance with birthdate in UserUpdate DTO

--- alabstest/src/main/java/com/example/alabstest/users/dto/UserUpdate.java
+++ alabstest/src/main/java/com/example/alabstest/users/dto/UserUpdate.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -14,5 +15,5 @@ import java.math.BigDecimal;
 public class UserUpdate {
     private String fullName;
     private String password;
-    private BigDecimal balance;
+    private LocalDate birthdate;
 }
