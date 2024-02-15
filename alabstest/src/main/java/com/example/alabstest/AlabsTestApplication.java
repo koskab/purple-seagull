16bc62e9756c2249ff7f6c8c4360054c3bb51c28
koskab <koskab13@icloud.com>
2024-02-15 10:32:14 +0600

    refactor: Autoconfigure files from IDE

--- alabstest/src/main/java/com/example/alabstest/AlabsTestApplication.java
+++ alabstest/src/main/java/com/example/alabstest/AlabsTestApplication.java
@@ -1,4 +1,4 @@
-package com.example.AlabsTest;
+package com.example.alabstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- alabstest/src/test/java/com/example/alabstest/AlabsTestApplicationTests.java
+++ alabstest/src/test/java/com/example/alabstest/AlabsTestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.AlabsTest;
+package com.example.alabstest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
